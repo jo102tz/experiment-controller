37276eef4b29c1fc88dd5e87675c26a21b456f21
Johannes Grohmann <jo102tz@users.noreply.github.com>
2020-03-25 15:51:34 +0100

    Update experiment1.sh

--- experiment1.sh
+++ experiment1.sh
@@ -3,3 +3,13 @@ echo "Starting setup for experiment 1..."
 
 echo "Start TeaStore application..."
 kubectl version --client
+
+echo "Press any key to continue"
+while [ true ] ; do
+read -t 3 -n 1
+if [ $? = 0 ] ; then
+exit ;
+else
+echo "waiting for the keypress"
+fi
+done
