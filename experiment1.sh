d9c176962520739c7e3dd68209dc2b87727ad04e
Johannes Grohmann <jo102tz@users.noreply.github.com>
2020-03-26 15:49:26 +0100

    Update experiment1.sh

--- experiment1.sh
+++ experiment1.sh
@@ -6,11 +6,3 @@ kubectl version --client
 kubectl apply -f /teastore/teastore-clusterip.yaml
 
 echo "Press any key to continue"
-while [ true ] ; do
-read -t 3 -n 1
-if [ $? = 0 ] ; then
-exit ;
-else
-echo "waiting for the keypress"
-fi
-done
