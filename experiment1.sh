c3fb350a79e30c2a08df6e4f5c34bee303b35dde
Johannes Grohmann <jo102tz@users.noreply.github.com>
2020-03-26 18:46:18 +0100

    Update experiment1.sh

--- experiment1.sh
+++ experiment1.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 echo "Starting setup for experiment 1..."
 
-echo "Start TeaStore application..."
 kubectl version --client
-kubectl apply -f /controller/teastore/teastore-clusterip.yaml
 
-echo "Press any key to continue"
+echo "Start TeaStore application..."
+kubectl create -f /controller/teastore/teastore-clusterip.yaml
+echo "Application up!"
+
+echo "Start load generator slaves..."
+kubectl create -f loadgenerator/loadgenerator-pod.yaml
+echo "Generators up!"
+
+echo "Setup complete!"
+
+echo "Tearing down after 20 seconds.."
+for i in {1..20}
+do
+echo "Waited for $i seconds..."
+sleep 1
+done
+
+echo "Start clean up..."
+kubectl delete -f /controller/teastore/teastore-clusterip.yaml
+kubectl delete -f loadgenerator/loadgenerator-pod.yaml
+
+echo "Clean up complete!"
+echo "Finished!"
+
