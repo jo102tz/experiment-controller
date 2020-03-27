887caac5903cae330243362c188514315cb8e028
Johannes Grohmann <jo102tz@users.noreply.github.com>
2020-03-27 10:57:03 +0100

    Update experiment1.sh

--- experiment1.sh
+++ experiment1.sh
@@ -8,7 +8,7 @@ kubectl create -f /controller/teastore/teastore-clusterip.yaml
 echo "Application up!"
 
 echo "Start load generator slaves..."
-kubectl create -f /controller/loadgenerator/loadgenerator-pod.yaml
+kubectl create -f /controller/loadgenerator/loadgenerator-slave-1.yaml
 echo "Generators up!"
 
 echo "Setup complete!"
@@ -21,12 +21,13 @@ sleep 1
 done
 
 echo "Starting load..."
-java -jar /controller/loadgenerator/httploadgenerator.jar director --ip 10.100.111.168 --load /controller/teastore/loads/increasingHighIntensity.csv -o testlog.csv --lua teastore_browse.lua
+java -jar /controller/loadgenerator/httploadgenerator.jar director --ip loadgenerator-slave-1 --load /controller/teastore/loads/increasingHighIntensity.csv -o testlog.csv --lua teastore_browse.lua
 
 
 echo "Start clean up..."
+kubectl delete -f /controller/loadgenerator/loadgenerator-slave-1.yaml
 kubectl delete -f /controller/teastore/teastore-clusterip.yaml
-kubectl delete -f /controller/loadgenerator/loadgenerator-pod.yaml
+
 
 echo "Clean up complete!"
 echo "Finished!"
