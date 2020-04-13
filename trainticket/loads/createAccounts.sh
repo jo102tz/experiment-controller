ccf5d7203856a1619ae48fdbbf007b6db32d669f
Johannes Grohmann <jo102tz@users.noreply.github.com>
2020-04-13 12:18:41 +0200

    Update createAccounts.sh

--- trainticket/loads/createAccounts.sh
+++ trainticket/loads/createAccounts.sh
@@ -4,7 +4,7 @@
 
 set +e
 # Admin Login
-curl -X POST -H "Content-Type: application/json" -d '{"name": "adminroot","password": "adminroot"}' http://10.1.3.43/account/adminlogin > temp.txt
+curl -X POST -H "Content-Type: application/json" -d '{"name": "adminroot","password": "adminroot"}' http://ts-ui-dashboard:8080/account/adminlogin > temp.txt
 
 # Extract Login UUID from response
 loginId=$(grep -zoP '"id":\s*\K[^\s,]*(?=\s*,)' temp.txt)
@@ -14,6 +14,6 @@ rm temp.txt
 # Create User Accounts
 for ((i = 1 ; i <= $1 ; i++)); do
     payload="{\"loginId\": $loginId, \"name\": \"user$i\", \"password\": \"user$i\", \"gender\": 1, \"email\": \"user$i@test.com\", \"documentType\": 1, \"documentNum\": 1}"
-    curl -X POST -H "Content-Type: application/json" -d "$payload" http://10.1.3.43/adminuser/addUser
+    curl -X POST -H "Content-Type: application/json" -d "$payload" http://ts-ui-dashboard:8080/adminuser/addUser
 done
 set -e
