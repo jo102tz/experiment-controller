8f122393227e357355534e9b2efb6f94a0ce5e4c
Johannes Grohmann <jo102tz@users.noreply.github.com>
2020-04-13 12:22:35 +0200

    Update experiment5.sh

--- experiment5.sh
+++ experiment5.sh
@@ -3,9 +3,9 @@ echo "Starting setup for experiment 1..."
 
 kubectl version --client
 
-echo "Start TeaStore application..."
-kubectl create -f /controller/teastore/teastore-clusterip.yaml
-echo "Application up!"
+#echo "Start TeaStore application..."
+#kubectl create -f /controller/teastore/teastore-clusterip.yaml
+#echo "Application up!"
 
 echo "Start load generator slaves..."
 kubectl create -f /controller/loadgenerator/loadgenerator-slave-1.yaml
@@ -32,7 +32,7 @@ java -jar /controller/loadgenerator/httploadgenerator.jar director --ip localhos
 
 echo "Start clean up..."
 kubectl delete -f /controller/loadgenerator/loadgenerator-slave-1.yaml
-kubectl delete -f /controller/teastore/teastore-clusterip.yaml
+#kubectl delete -f /controller/teastore/teastore-clusterip.yaml
 
 
 echo "Clean up complete!"
