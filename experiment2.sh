cd27b345634fbb2e13551251ef4653aa0723c035
Johannes Grohmann <jo102tz@users.noreply.github.com>
2020-04-13 12:21:44 +0200

    Update experiment2.sh

--- experiment2.sh
+++ experiment2.sh
@@ -28,8 +28,6 @@ java -jar /controller/httploadgenerator.jar loadgenerator &
 
 java -jar /controller/httploadgenerator.jar director --ip loadgenerator-slave-1 --load /controller/teastore/loads/increasingHighIntensity.csv -o testlog.csv --lua /controller/teastore/loads/teastore_browse.lua
 echo "Starting load..."
-java -jar /controller/httploadgenerator.jar director --ip 10.100.115.230 --load /controller/teastore/loads/increasingHighIntensity.csv -o testlog.csv --lua /controller/teastore/loads/teastore_browse.lua
-
 
 java -jar /controller/httploadgenerator.jar director --ip localhost --load /controller/teastore/loads/LongcloudArrivalRates140.csv -o testlog.csv --lua /controller/teastore/loads/teastore_browse.lua --timeout=3000
 
