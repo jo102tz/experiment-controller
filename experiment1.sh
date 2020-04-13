04c95a3b90d32c96a0835534c77d2d92842fc25e
Johannes Grohmann <jo102tz@users.noreply.github.com>
2020-04-13 11:34:33 +0200

    Update experiment1.sh

--- experiment1.sh
+++ experiment1.sh
@@ -23,15 +23,15 @@ done
 nslookup loadgenerator-slave-1
 nslookup teastore-webui
 
-java -jar /controller/loadgenerator/httploadgenerator.jar loadgenerator & 
+java -jar httploadgenerator.jar loadgenerator & 
 
 
-java -jar /controller/loadgenerator/httploadgenerator.jar director --ip loadgenerator-slave-1 --load /controller/teastore/loads/increasingHighIntensity.csv -o testlog.csv --lua /controller/teastore/loads/teastore_browse.lua
+java -jar httploadgenerator.jar director --ip loadgenerator-slave-1 --load /controller/teastore/loads/increasingHighIntensity.csv -o testlog.csv --lua /controller/teastore/loads/teastore_browse.lua
 echo "Starting load..."
-java -jar /controller/loadgenerator/httploadgenerator.jar director --ip 10.100.115.230 --load /controller/teastore/loads/increasingHighIntensity.csv -o testlog.csv --lua /controller/teastore/loads/teastore_browse.lua
+java -jar httploadgenerator.jar director --ip 10.100.115.230 --load /controller/teastore/loads/increasingHighIntensity.csv -o testlog.csv --lua /controller/teastore/loads/teastore_browse.lua
 
 
-java -jar /controller/loadgenerator/httploadgenerator.jar director --ip localhost --load /controller/teastore/loads/LongcloudArrivalRates140.csv -o testlog.csv --lua /controller/teastore/loads/teastore_browse.lua --timeout=3000
+java -jar httploadgenerator.jar director --ip localhost --load /controller/teastore/loads/LongcloudArrivalRates140.csv -o testlog.csv --lua /controller/teastore/loads/teastore_browse.lua --timeout=3000
 
 
 echo "Start clean up..."
